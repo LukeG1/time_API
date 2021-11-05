b430497cd690f0c454ffc13ea65118351dc559ad
Luke <lmgabel@gmail.com>
2021-11-05 11:30:27 -0400

    started to do some basic reading of the API

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,48 @@
 import React from "react";
 import './App.css';
-import { Counter } from "./components/Counter";
 import { TimeEntryHome } from "./components/TimeEntryHome";
-//import { Header } from "./components/Header";
 
+//MY TIME TRACKING API KEY: kiE3eTPhGN_8q3CpCvnRpQ
+export class App extends React.Component {
 
-//<Header title="hello from App" />
-function App() {
-  return (
-    <div className="App">
-      
-      <Counter startingValue={100}/>
-      <Counter startingValue={-100}/>
-      <TimeEntryHome 
-        name = {"this is a test"}
-        project = {"random project"}
-        start = {Date()}
-        end = {Date()}
-      />
+  state = {
+    curent_time: {}
+  }
 
-    </div>
-  );
+  componentDidMount(){
+      fetch('https://2020gabel.pythonanywhere.com/time_entries', {
+      mode: 'cors',
+      method: 'GET',
+      headers: {
+        "key": "kiE3eTPhGN_8q3CpCvnRpQ"
+      }
+    })
+    .then(res => res.json())
+    .then((data) => {
+      this.setState({ curent_time: 
+        {
+          name: data.data.description,
+          project: data.data.project_id,
+          start: data.data.start,
+          end: data.data.stop,
+        }
+      })
+    })
+    .catch(console.log)
+  }
+
+  render(){
+    console.log(this.state.curent_time.name)
+    return (
+      <div className="App">
+        
+        <TimeEntryHome 
+          data = {this.state.curent_time}
+        />
+
+      </div>
+    );
+  }
 }
 
 export default App;
--- frontend/src/components/TimeEntryHome.js
+++ frontend/src/components/TimeEntryHome.js
@@ -1,27 +1,15 @@
 import React from "react";
 
 export class TimeEntryHome extends React.Component {
-
-    constructor(props){
-        super(props);
-
-        this.state = {
-            name: props.name,
-            project: props.project,
-            start: props.start,
-            end: props.end,
-        }
-    }
-
     render() {
-        //style="display: flex; flex-direction: row;"
-      return (
-        <div >
-            <p>{ this.state.name }</p>
-            <p>{ this.state.project }</p>
-            <p>{ this.state.start }</p>
-            <p>{ this.state.end }</p>
-        </div>
-      );
-    }
+        return (
+            <div className="card">
+                <div className="card-body">
+                    <h5 className="card-title">{ this.props.data.name }</h5>
+                    <h6 className="card-subtitle mb-2 text-muted">{ this.props.data.project }</h6>
+                    <p className="card-text">{ this.props.data.start } - { this.props.data.end }</p>
+                </div>
+            </div>
+        );
+        }
   }
