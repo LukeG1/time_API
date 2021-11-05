c296fe5b0571bd95db47210395d57d93e65664ab
Luke <lmgabel@gmail.com>
2021-11-05 09:06:47 -0400

    added signature

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,6 +9,9 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <p>
+          I am testing that it really updates when I push
+        </p>
         <a
           className="App-link" 
           href="https://reactjs.org"
